d7303559b66121384585a9bdf15d9a976c317f8f
vittorione94 <vtrlbr94@gmail.com>
2023-02-14 18:05:35 +0000

    DEP noise

--- tonic/agents/__init__.py
+++ tonic/agents/__init__.py
@@ -1,5 +1,5 @@
 from .agent import Agent
-from .basic import Constant, NormalRandom, OrnsteinUhlenbeck, UniformRandom, SimulateActionNoise
+from .basic import Constant, NormalRandom, OrnsteinUhlenbeck, UniformRandom, DEP
 
 
-__all__ = [Agent, Constant, NormalRandom, OrnsteinUhlenbeck, UniformRandom, SimulateActionNoise]
+__all__ = [Agent, Constant, NormalRandom, OrnsteinUhlenbeck, UniformRandom, DEP]
--- tonic/explorations/__init__.py
+++ tonic/explorations/__init__.py
@@ -1,7 +1,7 @@
 from .noisy import NoActionNoise
 from .noisy import NormalActionNoise
 from .noisy import OrnsteinUhlenbeckActionNoise
-from .noisy import SimulateActionNoise
+from .noisy import DEPActionNoise
 
 
-__all__ = [NoActionNoise, NormalActionNoise, OrnsteinUhlenbeckActionNoise, SimulateActionNoise]
+__all__ = [NoActionNoise, NormalActionNoise, OrnsteinUhlenbeckActionNoise, DEPActionNoise]
