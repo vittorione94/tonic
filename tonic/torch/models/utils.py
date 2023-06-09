8d7ff53c46b47d7dcdeaea4518c3b5a743272b5e
vittorione94 <vtrlbr94@gmail.com>
2023-06-09 09:33:41 +0100

    torch fix

--- tonic/torch/models/utils.py
+++ tonic/torch/models/utils.py
@@ -13,7 +13,7 @@ class MLP(torch.nn.Module):
         layers = []
         for i in range(len(sizes) - 1):
             layers += [torch.nn.Linear(sizes[i], sizes[i + 1]),
-                       self.activation()]
+                       getattr(torch.nn, self.activation)()]
         self.model = torch.nn.Sequential(*layers)
         if self.fn is not None:
             self.model.apply(self.fn)
